3daba9e71df1c6f5613e157bf138faafc2e3dd8e
tianyuan <tianyuan@cisco.com>
2022-11-19 16:28:01 +0800

    add logging file

--- entrance.py
+++ entrance.py
@@ -2,7 +2,9 @@ from ise_cli_handler import ise_cli_handler
 import time
 
 if __name__ == "__main__":
+    logging = open("./output_log.txt", "w", encoding="utf-8")
     print("[Info] Script init")
+    logging.write("[Info] Script init\n")
     ise_ssh_session = ise_cli_handler()
 
     while True:
@@ -15,6 +17,7 @@ if __name__ == "__main__":
         if output_list[-1] == "ISE31-01/admin# ":
             # 如果不存在existing session
             print("[Info] no existing session")
+            logging.write("[Info] no existing session\n")
             break
         else:
             # 如果存在existing session
@@ -22,6 +25,7 @@ if __name__ == "__main__":
             # renew the connection
             ise_ssh_session = ise_cli_handler()
             print("[Warning] Destroy the existing session and create a new session")
+            logging.write("[Warning] Destroy the existing session and create a new session\n")
 
     # "enter key" sleep for 2 sec
     ise_ssh_session.remote_terminal.send("\n".encode())
@@ -40,6 +44,7 @@ if __name__ == "__main__":
         start to export the CA file if currently last line in ISE cli is "admin#"
         '''
         print("[Info] start to export the CA file")
+        logging.write("[Info] start to export the CA file\n")
         ise_ssh_session.remote_terminal.send("application configure ise\n".encode())
         ise_ssh_session.remote_terminal.send("7\n".encode())   # select the 7 to "export"
         time.sleep(5)
@@ -51,10 +56,12 @@ if __name__ == "__main__":
         # print(ise_ssh_session.remote_terminal.recv(65535).decode().split("\n"))
         if "Enter encryption-key for export:" in ise_ssh_session.remote_terminal.recv(65535).decode().split("\n")[-2]:
             print("[Info] exporting start, estimation timing 100s")
+            logging.write("[Info] exporting start, estimation timing 100s\n")
             time.sleep(100)
             ca_export_result_output = ise_ssh_session.remote_terminal.recv(65535).decode().split("\n")
         else:
             print("[Warning] Export Fail, please check the status of ISE and 'ise_info.json' file")
+            logging.write("[Warning] Export Fail, please check the status of ISE and 'ise_info.json' file\n")
             ise_ssh_session.remote_terminal.send("\x03\n".encode())
             ise_ssh_session.remote_terminal.send("\n".encode())
             ise_ssh_session.clear_existing_session()
@@ -64,20 +71,23 @@ if __name__ == "__main__":
         for line in ca_export_result_output:
             if "ISE CA keys export completed successfully" in line:
                 print("[Info] Export Successfully!")
+                logging.write("[Info] Export Successfully!\n")
                 ise_ssh_session.remote_terminal.send("0\n".encode())
                 ise_ssh_session.remote_terminal.send("\n".encode())
                 ise_ssh_session.remote_terminal.send("exit\n".encode())
                 break
             elif ca_export_result_output.index(line) == len(ca_export_result_output) - 1:
                 print("[Warning] Export Fail")
+                logging.write("[Warning] Export Fail\n")
                 ise_ssh_session.remote_terminal.send("0\n".encode())
                 ise_ssh_session.remote_terminal.send("\n".encode())
                 ise_ssh_session.remote_terminal.send("exit\n".encode())
                 break
     else:
         print("[Warning] Session Status is abnormal, session is terminated")
-
+        logging.write("[Warning] Session Status is abnormal, session is terminated\n")
 
     # destroy the session
     # print(ise_ssh_session.remote_terminal.recv(65535).decode())
+    logging.close()
     ise_ssh_session.ssh_session.close()
